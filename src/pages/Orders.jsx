7abb1468f1f1ece3c94e892dd9fa7b6395737714
zzayd30 <zaidlatif30@gmail.com>
2024-12-16 22:04:01 +0500

    Updated Orders Page

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -22,7 +22,7 @@ const Orders = ({ token }) => {
         }
       })
       if (response.data.success) {
-        setOrders(response.data.orders)
+        setOrders(response.data.orders.reverse())
       }
       else {
         toast.error(response.data.message)
